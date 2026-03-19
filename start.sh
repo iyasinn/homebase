35af3db8cd2f82d3acf5411dc59bbcc2c8b91a54
iyasin <iyasin@umich.edu>
2026-03-19 07:53:12 -0400

    support nextcloud

--- start.sh
+++ start.sh
@@ -4,4 +4,7 @@
 echo "Starting core services..."
 docker compose -f services/core/docker-compose.yml up -d
 
+echo "Starting storage services..."
+docker compose -f services/storage/docker-compose.yml up -d
+
 echo "Done!"
--- stop.sh
+++ stop.sh
@@ -2,4 +2,7 @@
 # ~/homebase/stop.sh
 
 # Core turns off last
+docker compose -f services/storage/docker-compose.yml down
+
+# Core turns off last
 docker compose -f services/core/docker-compose.yml down
