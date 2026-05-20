2efa93c691f81ccba703444053571130e09213bb
Ihsaan Yasin <iayasin771@gmail.com>
2026-05-20 12:45:36 -0400

    update stop.sh

--- stop.sh
+++ stop.sh
@@ -2,7 +2,7 @@
 # ~/homebase/stop.sh
 
 # Core turns off last
-docker compose -f services/storage/docker-compose.yml down
+docker compose --env-file .env -f services/storage/docker-compose.yml down
 
 # Core turns off last
-docker compose -f services/core/docker-compose.yml down
+docker compose --env-file .env -f services/core/docker-compose.yml down
